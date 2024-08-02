15e72223449262c0f289811319bc0afdecaa28fe
RB <renatabezerratecnologia@gmail.com>
2024-08-02 20:54:12 -0300

    update readme

--- insere-dados-tabela-db_testeConexao.py
+++ insere-dados-tabela-db_testeConexao.py
@@ -7,23 +7,23 @@ engine = create_engine(
 
 def inserir_dados():
     # Solicita os dados ao usuário
-    id = input("Digite o ID (número inteiro): ")
     nome = input("Digite o Nome: ")
     idade = input("Digite a Idade (número inteiro): ")
 
     # Verifique se os valores inseridos estão corretos
-    print(f"ID: {id}, Nome: {nome}, Idade: {idade}")
+    print(f"Nome: {nome}, Idade: {idade}")
     
     # Monta a consulta SQL para inserir os dados
     consulta = text("""
-        INSERT INTO exemplo (id, nome, idade)
-        VALUES (:id, :nome, :idade)
+        INSERT INTO exemplo (nome, idade)
+        VALUES (:nome, :idade)
     """)
     
      # Executa a consulta SQL
     with engine.connect() as connection:
         try:
-            connection.execute(consulta, {"id": int(id), "nome": nome, "idade": int(idade)})
+            connection.execute(consulta, { "nome": nome, "idade": idade})
+            connection.commit()
             print("Dados inseridos com sucesso!")
         except Exception as e:
             print(f"Erro ao inserir dados: {e}")
