3d674d8996dcf9e838b6ba4edec42dc42133cf2a
RB <renatabezerratecnologia@gmail.com>
2024-08-17 15:01:24 -0300

    Organização de tarefa futura

--- Data manipulation/data_warehouse.py
+++ Data manipulation/data_warehouse.py
@@ -1,4 +1,6 @@
-
+# Coleção de dados orientados por assunto, integrados, variáveis com o tempo e não voláteis ---> para dar suporte ao processo de tomada de decisão.
+# Autor do conceito: Bill Inmon 
 # OLAP
 # Data Warehouse - análise
 # Dados: inclusão e leitura
+
